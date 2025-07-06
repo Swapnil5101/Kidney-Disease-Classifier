baa0eeb82c0e5d7029d4bb22129338a1b7c9c0a4
Swapnil5101 <swapnil.nagar4774@gmail.com>
2025-07-06 23:41:54 +0530

    Data Ingestion completed

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from CNN_Classifier import logger
+from CNN_Classifier.pipeline.data_ingestion_stage1 import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n------------")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/CNN_Classifier/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+# CONFIG_FILE_PATH = "config/config.yaml"
+# PARAMS_FILE_PATH = "params.yaml"
+PARAMS_FILE_PATH = Path("params.yaml")
